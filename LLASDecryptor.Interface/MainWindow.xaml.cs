3634e23e8c2ac2c796dc4756ebc971a73edc18c8
Bryson Ewell <brysonewell@outlook.com>
2022-02-21 18:46:07 -0500

    Added databinding, saving, and loading data.

--- LLASDecryptor.Interface/MainWindow.xaml.cs
+++ LLASDecryptor.Interface/MainWindow.xaml.cs
@@ -1,8 +1,12 @@
 ﻿using LLASDecryptor.Core;
+using LLASDecryptor.Interface.Properties;
+using Microsoft.Win32;
+using Microsoft.WindowsAPICodePack.Dialogs;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
+//using System.Windows.Forms;
 
 namespace LLASDecryptor.Interface
 {
@@ -11,28 +15,19 @@ namespace LLASDecryptor.Interface
     /// </summary>
     public partial class MainWindow : Window
     {
-        public string InputPath { get; set; } = @"L:\SIFAS\Bluestacks\files\files";
+        public readonly DependencyProperty InputProp = DependencyProperty.Register(nameof(InputPath), typeof(string), typeof(MainWindow));
+        public readonly DependencyProperty OutputProp = DependencyProperty.Register(nameof(OutputPath), typeof(string), typeof(MainWindow));
+        public readonly DependencyProperty PlayerPrefsProp = DependencyProperty.Register(nameof(PlayerPrefsKey), typeof(string), typeof(MainWindow));
 
-        public string OutputPath { get; set; } = @"L:\SIFAS\output";
+        public string InputPath { get => (string)GetValue(InputProp); set => SetValue(InputProp, value); }
+        public string OutputPath { get => (string)GetValue(OutputProp); set => SetValue(OutputProp, value); }
+        public string PlayerPrefsKey { get => (string)GetValue(PlayerPrefsProp); set => SetValue(PlayerPrefsProp, value); }
 
-        private double currentProgress = 0;
-
-        private object progressLock = new object();
-
-        private List<string> fileTables = new List<string>()
-        {
-            "member_model",
-            "navi_motion",
-            "navi_timeline",
-            "live_timeline",
-            "stage",
-            "stage_effect",
-            "member_facial_animation",
-            "member_facial",
-        };
+        private List<string> fileTables = new List<string>();
 
         public MainWindow()
         {
+            LoadSettings();
             InitializeComponent();
             DecryptProgress.Value = 0;
             var dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
@@ -45,14 +40,34 @@ namespace LLASDecryptor.Interface
             dispatcherTimer.Start();
         }
 
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            SaveSettings();
+        }
+
+        void LoadSettings()
+        {
+            InputPath = Settings.Default.InputFiles;
+            OutputPath = Settings.Default.OutputFiles;
+            PlayerPrefsKey = Settings.Default.PlayerPrefsKey;
+        }
+
+        void SaveSettings()
+        {
+            Settings.Default.InputFiles = InputPath;
+            Settings.Default.OutputFiles = OutputPath;
+            Settings.Default.PlayerPrefsKey = PlayerPrefsKey;
+            Settings.Default.Save();
+        }
+
         private async void FileButton_Click(object sender, RoutedEventArgs e)
         {
-            Decryptor decryptor = new Decryptor(InputPath, OutputPath);
+            Decryptor decryptor = BuildDecryptor();
             decryptor.ProgressChanged += ProgressChanged;
             decryptor.ConsoleLog += LogConsole;
 
             var listBoxItems = new List<string>(TablesList.SelectedItems.Cast<string>());
-            fileTables = listBoxItems;//.Select(lb => lb..ToString()).ToList();// DatabaseTables.Tables.Select(t => t.TableName).ToList();
+            fileTables = listBoxItems;
 
             if(fileTables.Count == 0)
             {
@@ -69,8 +84,14 @@ namespace LLASDecryptor.Interface
                 ProgressText.Text = $"Processing '{tableName}'";
 
                 var table = DatabaseTables.Tables.Find(t => t.TableName == tableName);
-
-                await decryptor.DecryptFiles(table);
+                try
+                {
+                    await decryptor.DecryptFiles(table);
+                }
+                catch(Exception ex)
+                {
+                    LogConsole(ex.Message + ":" + ex.StackTrace);
+                }
                 tablesCompleted++;
                 UpdateOverallProgress(tablesCompleted, totalTables);
             }
@@ -87,7 +108,7 @@ namespace LLASDecryptor.Interface
 
         private void DatabaseButton_Click(object sender, RoutedEventArgs e)
         {
-            Decryptor decryptor = new Decryptor(InputPath, OutputPath);
+            Decryptor decryptor = BuildDecryptor();
             decryptor.DecryptDatabase();
         }
 
@@ -102,6 +123,29 @@ namespace LLASDecryptor.Interface
         private void UpdateOverallProgress(int tablesCompleted, int totalTables)
         {
             OverallProgress.Value = (double)tablesCompleted / totalTables;
+        }
+
+        private Decryptor BuildDecryptor()
+        {
+            return new Decryptor(InputPath, OutputPath, PlayerPrefsKey);
+        }
+
+        private void InputFolderSelect_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new CommonOpenFileDialog();
+            dialog.Title = "Input Folder";
+            dialog.IsFolderPicker = true;
+            dialog.ShowDialog();
+            InputPath = dialog.FileName;
+        }
+
+        private void OutputFolderSelect_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new CommonOpenFileDialog();
+            dialog.Title = "Output Folder";
+            dialog.IsFolderPicker = true;
+            dialog.ShowDialog();
+            OutputPath = dialog.FileName;
         }
     }
 }
